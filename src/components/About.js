42344330f59935f3243f563e7310dfdcfdf4051f
Ali Rıza <ars.lifestyle@hotmail.com>
2021-06-12 19:49:11 +0300

    Finish the lab by fixing all yhe import/export problems between components.

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,15 @@
 import React from "react";
+import * as userData from "../data/user.js"
+
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
       <p>I Made This</p>
-      <img src={image} alt="I made this" />
+      <img src={userData.image} alt="I made this" />
     </div>
   );
 }
+
+export default About
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
+// import username from "../data/user.js";
+// import city from "../data/user.js";
+import * as userData from "../data/user.js"
 
 function Home() {
   return (
     <div id="home">
       <h1>
-        {username} is a Web Developer from {city}
+        {userData.username} is a Web Developer from {userData.city}
       </h1>
     </div>
   );
 }
+
+export default Home
